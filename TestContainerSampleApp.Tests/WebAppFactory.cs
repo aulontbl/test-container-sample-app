56545ccac0e7037a5eb352550d2cf1ea621ab775
Aulon Gashi <aulon.gashi@egeli-informatik.ch>
2026-05-01 16:34:05 +0200

    feat: edit webapp factory

---
+++ TestContainerSampleApp.Tests/WebAppFactory.cs
@@ -0,0 +1,52 @@
+﻿using Alba;
+using AwesomeAssertions;
+
+namespace TestContainerSampleApp.Tests;
+using Testcontainers.Oracle;
+
+public class WebAppFactory : IAsyncLifetime
+{
+    private readonly OracleContainer _container = new OracleBuilder()
+        .WithImage("gvenzl/oracle-free:23.4-slim-faststart")
+        .Build();
+    
+    public Task InitializeAsync()
+    {
+        return _container.StartAsync();
+    }
+    public Task DisposeAsync()
+    {
+        return _container.StopAsync();
+    }
+    
+    
+    
+    
+    
+
+    
+    
+    
+}
+
+public class Foo
+{
+    [Fact]
+    public async Task TestSomething()
+    {
+        // Alba will automatically manage the lifetime of the underlying host
+        await using var host = await AlbaHost.For<global::Program>();
+    
+        // This runs an HTTP request and makes an assertion
+        // about the expected content of the response
+        var result = await host.Scenario(_ =>
+        {
+            _.Get.Url("/sample");
+            _.StatusCodeShouldBeOk();
+        });
+
+
+        var content = await result.ReadAsTextAsync();
+        content.Should().Be("Hello World!");
+    }
+}
--- TestContainerSampleApp/Program.cs
+++ TestContainerSampleApp/Program.cs
@@ -1,45 +1,16 @@
-using Microsoft.EntityFrameworkCore;
 using TestContainerSampleApp;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-{
-    options.UseOracle(builder.Configuration.GetConnectionString("Oracle"));
-});
+
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
+app.MapGet("/sample", () => "Hello World!");
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-    {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
-    })
-    .WithName("GetWeatherForecast");
 
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
+public record Coder(int Id, string Name, string Language, string Architecture, bool IsVibeCoder);
