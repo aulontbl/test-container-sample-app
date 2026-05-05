9821a82070775a13f417f1d77a77c1325ab4b3ae
Aulon Gashi <aulon.gashi@egeli-informatik.ch>
2026-05-05 15:54:59 +0200

    feat: add nhibernate mapping & mstest proj

---
+++ TestContainerSampleApp.Test/OracleIntegrationTests.cs
@@ -0,0 +1,24 @@
+﻿namespace TestContainerSampleApp.Test;
+using Testcontainers.Oracle;
+
+[TestClass]
+[TestCategory("Integration")]
+public class OracleIntegrationTests
+{
+    private static OracleBuilder _oracle;
+    
+    [ClassInitialize]
+    public static async Task ClassInitialize(TestContext context)
+    {
+        _oracle = new OracleBuilder("gvenzl/oracle-free:23.4-slim-faststart");
+        await _oracle.StartAsync();
+    }
+    
+    [ClassCleanup]
+    public static async Task ClassCleanup()
+    {
+        await _oracle.StopAsync();
+    }
+
+    
+}
---
+++ TestContainerSampleApp/CoderMap.cs
@@ -0,0 +1,41 @@
+﻿using NHibernate;
+using NHibernate.Mapping;
+using NHibernate.Mapping.ByCode;
+using NHibernate.Mapping.ByCode.Conformist;
+
+namespace TestContainerSampleApp;
+
+public class CoderMap : ClassMapping<Coder>
+{
+    public CoderMap()
+    {
+        Table("coders");
+
+        Id(c => c.Id, map =>
+        {
+            map.Column("id");
+            map.Generator(Generators.Identity);
+        });
+        
+        Discriminator(d =>
+        {
+            d.Column("type");
+            d.Type(NHibernateUtil.String);
+        });
+        
+        Property(c => c.Name, map => map.Column("name"));
+        Property(c => c.Language, map => map.Column("language"));
+        Property(c => c.Architecture, map => map.Column("architecture"));
+        Property(c => c.IsVibeCoder, map => map.Column("is_vibe_coder"));
+        
+
+    }
+}
+
+public class AdisCoderMap : SubclassMapping<AdisCoder>
+{
+    public AdisCoderMap()
+    {
+        DiscriminatorValue("adis_coder");
+    }
+}
